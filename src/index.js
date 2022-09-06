0c602ee03d1e4ae0386f1d0fa08089583bfd553e
Oyahebholo kenneth Izosea <83974288+kennethoyahebholo@users.noreply.github.com>
2022-09-06 16:14:30 +0100

    update

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider} from "react-redux";
 import store from '../src/app/store'
+require('dotenv').config();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
